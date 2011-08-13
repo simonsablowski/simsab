3fddb069b2b80e9340fc035db6f36bdcebeba38a
simsab@simsab.de <simsab@simsab.de@be2d0dbf-0e9b-095f-fa13-e996d603e40d>
2011-08-13 13:00:14 +0000

    corrected project dates

--- trunk/views/index.php
+++ trunk/views/index.php
@@ -275,7 +275,7 @@
 							</dt>
 							<dd class="description">
 								<p>
-									<?php echo $this->localize('One-day project'); ?>: <?php echo $this->localize('Ask and answer questions'); ?> (2010) &ndash;
+									<?php echo $this->localize('One-day project'); ?>: <?php echo $this->localize('Ask and answer questions'); ?> (2011) &ndash;
 									<a href="http://code.google.com/p/askme/source/browse/trunk" title="<?php echo $this->localize('Source code'); ?>"><?php echo $this->localize('Source code'); ?></a>
 								</p>
 							</dd>
@@ -284,7 +284,7 @@
 							</dt>
 							<dd class="description">
 								<p>
-									<?php echo $this->localize('One-day project'); ?>: <?php echo $this->localize('Multi-format instant messaging dialogues'); ?> (2010) &ndash;
+									<?php echo $this->localize('One-day project'); ?>: <?php echo $this->localize('Multi-format instant messaging dialogues'); ?> (2011) &ndash;
 									<a href="http://code.google.com/p/dialogues/source/browse/trunk" title="<?php echo $this->localize('Source code'); ?>"><?php echo $this->localize('Source code'); ?></a>
 								</p>
 							</dd>
