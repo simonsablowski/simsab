e25d0df924e044f16a5ff96d79d9425579ab5e9f
Simon Sablowski <contact@simsab.net>
2013-11-01 15:07:33 +0000

    - making full tracking code configurable - moving custom font declaration to stylesheet - minor improvements

--- configuration.php
+++ configuration.php
@@ -30,6 +30,9 @@ $configuration['Request'] = array(
 	'aliasQueries' => array()
 );
 
-$configuration['trackerCode'] = 'UA-9712670-1';
+date_default_timezone_set('Europe/Berlin');
+$configuration['currentYear'] = date('Y');
+
+$configuration['trackingCode'] = "\t\t<script type=\"text/javascript\">\n\t\t<!--\n\t\tvar gaJsHost = 'https:' == document.location.protocol ? 'https://ssl.' : 'http://www.';\n\t\tdocument.write(unescape('%3Cscript src=\"' + gaJsHost + 'google-analytics.com/ga.js\" type=\"text/javascript\"%3E%3C/script%3E'));\n\t\t//-->\n\t\t</script>\n\t\t<script type=\"text/javascript\">\n\t\t<!--\n\t\ttry {\n\t\t\tvar pageTracker = _gat._getTracker('UA-9712670-1');\n\t\t\tpageTracker._initData();\n\t\t\tpageTracker._trackPageview();\n\t\t} catch (error) {\n\t\t\t\n\t\t}\n\t\t//-->\n\t\t</script>";
 $configuration['pathSeparator'] = '#';
 $configuration['debugMode'] = FALSE;
