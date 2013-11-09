2e0073b2b97bd0f0babedde777c35a04498f1991
simbobwe@googlemail.com <simbobwe@googlemail.com@be2d0dbf-0e9b-095f-fa13-e996d603e40d>
2013-11-09 13:30:42 +0000

    - adding proper view for legal-notice page - removing static html file impressum.html - adding styles for legal-notice page - moving legal-notice translations to translation file (de.po only) - minor improvements to translation files

---
+++ trunk/views/legal-notice.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->localize('document-language'); ?>">
+	<head>
+		<title><?php echo $this->localize('document-title'); ?></title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<meta name="robots" content="noindex, nofollow"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+		<link href="<?php echo $this->localize('canonical-url'); ?>" rel="canonical"/>
+		<link href="<?php echo $this->getConfiguration('basePath'); ?>css/simsab.css" rel="stylesheet" type="text/css"/>
+		<link href="<?php echo $this->getConfiguration('basePath'); ?>img/simsab.ico" rel="shortcut icon" type="image/x-icon"/>
+	</head>
+	<body>
+		<div id="document">
+			<div id="header">
+				<a href="<?php echo $this->getConfiguration('basePath') . $this->localize('document-language'); ?>" title="<?php echo $this->localize('document-title'); ?>"><img id="logo" src="<?php echo $this->getConfiguration('basePath'); ?>img/logo.png" alt="<?php echo $this->localize('document-title'); ?>" title="<?php echo $this->localize('document-title'); ?>"/></a>
+			</div>
+			<div id="sidebar">
+				<ul class="menu items">
+					<li class="item">
+						<a class="inactive" href="<?php echo $this->getConfiguration('basePath') . $this->localize('document-language') . $this->getConfiguration('pathSeparator') . $this->localize('about-me-path'); ?>" title="<?php echo $this->localize('about-me-menu-title'); ?>"><?php echo $this->localize('about-me-menu-title'); ?></a>
+					</li>
+					<li class="item">
+						<a class="inactive" href="<?php echo $this->getConfiguration('basePath') . $this->localize('document-language') . $this->getConfiguration('pathSeparator') . $this->localize('qualities-path'); ?>" title="<?php echo $this->localize('qualities-menu-title'); ?>"><?php echo $this->localize('qualities-menu-title'); ?></a>
+					</li>
+					<li class="item">
+						<a class="inactive" href="<?php echo $this->getConfiguration('basePath') . $this->localize('document-language') . $this->getConfiguration('pathSeparator') . $this->localize('career-path'); ?>" title="<?php echo $this->localize('career-menu-title'); ?>"><?php echo $this->localize('career-menu-title'); ?></a>
+					</li>
+					<li class="item">
+						<a class="inactive" href="<?php echo $this->getConfiguration('basePath') . $this->localize('document-language') . $this->getConfiguration('pathSeparator') . $this->localize('projects-path'); ?>" title="<?php echo $this->localize('projects-menu-title'); ?>"><?php echo $this->localize('projects-menu-title'); ?></a>
+					</li>
+					<li class="item">
+						<a class="inactive" href="<?php echo $this->getConfiguration('basePath') . $this->localize('document-language') . $this->getConfiguration('pathSeparator') . $this->localize('contact-me-path'); ?>" title="<?php echo $this->localize('contact-me-menu-title'); ?>"><?php echo $this->localize('contact-me-menu-title'); ?></a>
+					</li>
+				</ul>
+			</div>
+			<div id="body">
+				<div class="section legal-notice">
+					<h2 class="title">
+						<a href="<?php echo $this->getConfiguration('basePath') . $this->localize('legal-notice-path'); ?>" title="<?php echo $this->localize('legal-notice-title'); ?>" rel="nofollow"><?php echo $this->localize('legal-notice-title'); ?></a>
+					</h2>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block1-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block1-paragraph1'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block2-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block2-paragraph1'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block3-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block3-paragraph1'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block4-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block4-paragraph1'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block5-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block5-paragraph1'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block6-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block6-paragraph1'); ?>
+
+					</p>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block6-paragraph2'); ?>
+
+					</p>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block6-paragraph3'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block7-title'); ?>
+
+					</h3>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block7-paragraph1'); ?>
+
+					</p>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block7-paragraph2'); ?>
+
+					</p>
+					<p class="paragraph">
+						<?php echo $this->localize('legal-notice-block7-paragraph3'); ?>
+
+					</p>
+					<h3 class="title">
+						<?php echo $this->localize('legal-notice-block8-title'); ?>
+
+					</h3>
+					<ul>
+						<li>
+							<?php echo $this->localize('legal-notice-block8-bullet1'); ?>
+
+						</li>
+						<li>
+							<?php echo $this->localize('legal-notice-block8-bullet2'); ?>
+
+						</li>
+						<li>
+							<?php echo $this->localize('legal-notice-block8-bullet3'); ?>
+
+						</li>
+						<li>
+							<?php echo $this->localize('legal-notice-block8-bullet4'); ?>
+
+						</li>
+						<li>
+							<?php echo $this->localize('legal-notice-block8-bullet5'); ?>
+
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
