933bf9fcdd0650f3c8b5d5212ba57b5053467650
Simon Sablowski <contact@simsab.net>
2016-09-06 17:49:48 +0200

    major update fall 2016

---
+++ js/simsab.js
@@ -0,0 +1,5 @@
+$('.collapse').on('shown.bs.collapse', function() {
+	$(this).parent().find('.fa-plus').removeClass('fa-plus').addClass('fa-minus');
+}).on('hidden.bs.collapse', function() {
+	$(this).parent().find('.fa-minus').removeClass('fa-minus').addClass('fa-plus');
+});
