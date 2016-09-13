ae22133b2b784e3845f2b2b458c2d58c14d96da1
Simon Sablowski <contact@simsab.net>
2016-09-13 13:03:46 +0200

    improvements and fixes
    merged print and screen stylesheets, improved print version, added
    function for calculating years with current employer, fixed burger menu
    and accordion for mobile and print versions

--- js/simsab.js
+++ js/simsab.js
@@ -3,3 +3,13 @@ $('.collapse').on('shown.bs.collapse', function() {
 }).on('hidden.bs.collapse', function() {
 	$(this).parent().find('.fa-minus').removeClass('fa-minus').addClass('fa-plus');
 });
+
+function yearsSince(dateString) {
+	var dateDiff = new Date(Date.now() - new Date(dateString).getTime());
+	var years = dateDiff.getFullYear() - 1970;
+	return years != 1 ? years + ' years' : years + ' year';
+}
+
+$('.years-since').each(function() {
+	$(this).html(yearsSince($(this).data('date')));
+});
