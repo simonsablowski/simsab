38ebac0b880083c23068faa28993251e75d059e3
Simon Sablowski <contact@simsab.net>
2018-11-04 00:00:04 +0100

    redesign
    - simplifying content
    - removing unnecessary functionality
    - updating colour palette and fonts

--- js/simsab.js
+++
@@ -1,5 +0,0 @@
-$('.collapse').on('shown.bs.collapse', function() {
-	$(this).parent().find('.fa-plus').removeClass('fa-plus').addClass('fa-minus');
-}).on('hidden.bs.collapse', function() {
-	$(this).parent().find('.fa-minus').removeClass('fa-minus').addClass('fa-plus');
-});
